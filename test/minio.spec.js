8c15d34560a3a308b08b5bba67e28635f8ebfe2b
al66 <andreas.leinen@imicros.de>
2020-09-19 09:39:26 +0200

    update dependencies + correction decryption pipe

--- test/minio.spec.js
+++ test/minio.spec.js
@@ -12,7 +12,12 @@ const Keys = {
     name: "keys",
     actions: {
         getOek: {
+            params: {
+                service: { type: "string" },
+                id: { type: "string", optional: true }
+            },
             handler(ctx) {
+                ctx.broker.logger.info("getOek", { params: ctx.params });
                 if (!ctx.params || !ctx.params.service) throw new Error("Missing service name");
                 if ( ctx.params.id == "prev" ) {
                     return {
@@ -30,6 +35,7 @@ const Keys = {
     created() {
         this.prev = uuid();
         this.current = uuid();
+        this.broker.logger.info("keys", { current: this.current, previous: this.prev });
     } 
 };
 
